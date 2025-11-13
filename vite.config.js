97912187f9ecb4e07b4fcd6fe96a4ad4b8515d11
Narihito <heinboss234@gmail.com>
2025-11-13 13:19:54 +0630

    f

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/MaLan-Ai/",
   build: {
-    outDir: 'build'
+    outDir: 'build',
   }
 });
